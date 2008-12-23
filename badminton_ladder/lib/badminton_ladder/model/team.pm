b9e2fac24b634c6af62d870c7451e6b1efef3c07
Andy Brown <ajb@sanger.ac.uk>
2008-12-23 16:21:40 +0000

    teams and person added at the same time

--- badminton_ladder/lib/badminton_ladder/model/team.pm
+++ badminton_ladder/lib/badminton_ladder/model/team.pm
@@ -3,6 +3,10 @@ package badminton_ladder::model::team;
 use strict;
 use warnings;
 use base qw(ClearPress::model);
+use Carp qw(carp cluck croak confess);
+use English qw{-no_match_vars};
+use badminton_ladder::model::player;
+use badminton_ladder::model::player_team;
 
 __PACKAGE__->mk_accessors(__PACKAGE__->fields());
 __PACKAGE__->has_a([qw()]);
@@ -22,6 +26,45 @@ sub played {
     $self->{played} = $self->loss() + $self->win();
   }
   return $self->{played};
+}
+
+sub generate_team {
+  my ($self, $arg_ref) = @_;
+  my $util = $self->util();
+  eval {
+    my $player_1 = badminton_ladder::model::player->new({
+      util => $util,
+      forename => $arg_ref->{player_1}->{forename},
+      surname => $arg_ref->{player_1}->{surname},
+      email => $arg_ref->{player_1}->{email},
+    });
+    $player_1->create();
+    $player_1->read();
+    my $player_2 = badminton_ladder::model::player->new({
+      util => $util,
+      forename => $arg_ref->{player_2}->{forename},
+      surname => $arg_ref->{player_2}->{surname},
+      email => $arg_ref->{player_2}->{email},
+    });
+    $player_2->create();
+    $player_2->read();
+    my $player_1_team = badminton_ladder::model::player_team->new({
+      util => $util,
+      id_player => $player_1->id_player(),
+      id_team => $self->id_team(),
+    });
+    my $player_2_team = badminton_ladder::model::player_team->new({
+      util => $util,
+      id_player => $player_2->id_player(),
+      id_team => $self->id_team(),
+    });
+    $player_1_team->save();
+    $player_2_team->save();
+    1;
+  } or do {
+    confess $EVAL_ERROR;
+  };
+  return 1;
 }
 
 1;
--- badminton_ladder/lib/badminton_ladder/view/team.pm
+++ badminton_ladder/lib/badminton_ladder/view/team.pm
@@ -4,5 +4,32 @@ use strict;
 use warnings;
 use base qw(ClearPress::view);
 
+sub create {
+  my ($self) = @_;
+  my $cgi = $self->util->cgi();
+  my $name = $cgi->param('name');
+  my $forename_1 = $cgi->param('forename_1');
+  my $surname_1 = $cgi->param('surname_1');
+  my $email_1 = $cgi->param('email_1');
+  my $forename_2 = $cgi->param('forename_2');
+  my $surname_2 = $cgi->param('surname_2');
+  my $email_2 = $cgi->param('email_2');
+  my $model = $self->model();
+  $name ||= $surname_1.q{/}.$surname_2;
+  $model->name($name);
+  $model->win(0);
+  $model->loss(0);
+  eval {
+    $model->create();
+    $model->generate_team({
+      player_1 => { forename => $forename_1, surname => $surname_1, email => $email_1 },
+      player_2 => { forename => $forename_2, surname => $surname_2, email => $email_2 },
+    });
+  } or do {
+    croak $EVAL_ERROR;
+  };
+  return 1;
+}
+
 1;
  
