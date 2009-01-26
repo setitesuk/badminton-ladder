d7abf59fefc0aaf2fe30abb60e9f276f839d5db9
Andy Brown <ajb@sanger.ac.uk>
2009-01-26 15:43:26 +0000

    Aim to reduce headachew when trying to view information

--- badminton_ladder/lib/badminton_ladder/model/player.pm
+++ badminton_ladder/lib/badminton_ladder/model/player.pm
@@ -8,6 +8,7 @@ __PACKAGE__->mk_accessors(__PACKAGE__->fields());
 __PACKAGE__->has_a([qw()]);
 __PACKAGE__->has_many([qw(player_team )]);
 __PACKAGE__->has_all();
+__PACKAGE__->has_many_through('team|player_team');
 
 sub fields {
   return qw(id_player email forename surname);
